2c241e17c34856310136903f746dae059e5a6a30
Matthew Yang <imatthewyang@gmail.com>
2014-03-31 20:25:30 -0700

    added a test class

---
+++ TMRWTest.h
@@ -0,0 +1,13 @@
+//
+//  TMRWTest.h
+//  Git Test
+//
+//  Created by Matthew Yang on 3/31/14.
+//  Copyright (c) 2014 Tmrw, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TMRWTest : NSObject
+
+@end
---
+++ TMRWTest.m
@@ -0,0 +1,13 @@
+//
+//  TMRWTest.m
+//  Git Test
+//
+//  Created by Matthew Yang on 3/31/14.
+//  Copyright (c) 2014 Tmrw, Inc. All rights reserved.
+//
+
+#import "TMRWTest.h"
+
+@implementation TMRWTest
+
+@end
